9af9790b223b7a09006bb47c7db5321672c81761
kashyapvishal <168442172+18april@users.noreply.github.com>
2024-08-06 07:35:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('HELLO WORLD FROM K8s vishal testing the ');
+  res.send('HELLO WORLD FROM K8s vishal testing the kion');
 });
 
 app.listen(PORT, HOST, () => {
